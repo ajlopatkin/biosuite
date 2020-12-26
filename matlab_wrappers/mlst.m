d0025653dcc411a3e7107cf7ecd72706cabf543a
csjohnson23 <75772305+csjohnson23@users.noreply.github.com>
2020-12-26 21:26:06 +0000

    add database argument

--- matlab_wrappers/mlst.m
+++ matlab_wrappers/mlst.m
@@ -1,6 +1,6 @@
-function mlst(input_fasta,output_path)
+function mlst(input_fasta,output_path,database)
 
-str = "bash $BIOSUITE_HOME/bash_source/mlst.sh " + input_fasta + " " + output_path;
+str = "bash $BIOSUITE_HOME/bash_source/mlst.sh " + input_fasta + " " + output_path + " " + database;
 
 prev_path = cd(output_path);
 [status,cmdout] = system(str);
