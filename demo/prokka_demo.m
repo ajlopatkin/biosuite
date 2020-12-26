f2d5b641fea7640d8d473d4283e7da91b5ca063e
csjohnson23 <75772305+csjohnson23@users.noreply.github.com>
2020-12-26 22:41:06 +0000

    Add genus customization to prokka_demo

--- demo/prokka_demo.m
+++ demo/prokka_demo.m
@@ -2,6 +2,7 @@
 % % #	+ 1. prefix_name - file prefix (must be same file name as in input_fasta)
 % % #	+ 2. input_fasta - Path for input file to be annotated
 % % #	+ 3. output_dir - Directory path for storing output files
+% % #	+ 4. genus - Organism genus: Enterococcus, Escherichia, or Staphylococcus
 
 clear;close all;clc
 BasePath = getenv("BIOSUITE_HOME");
@@ -15,6 +16,9 @@ input_fasta = BasePath + "/demo/demo_data/" + prefix_name + ".fasta";
 
 %Input 3: Directory path for output 
 output_dir = BasePath + "/demo/output/prokka/";
+
+%Input 4: Organism genus
+genus = "Escherichia"
 %%%%%%%%%%%%%%%%%%%%%%
 
 %%%%%%%% DEMO %%%%%%%%
