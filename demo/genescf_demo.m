1394fbe94b290b47cd83318aa44658b82bbddfc4
csjohnson23 <75772305+csjohnson23@users.noreply.github.com>
2020-12-26 17:58:15 +0000

    Update genescf_demo.m

--- demo/genescf_demo.m
+++ demo/genescf_demo.m
@@ -1,17 +1,26 @@
 % This demo runs enrichment analysis on an input set of genes
 
 % % % # INPUTS (IN ORDER): 
-% % % #	+ 1. input_file- input aligned file
-% % % # + 2. output_dir- output newick tree
-% % % # + 3. db_type- KEGG, GO, or GO_BP
+% % % #	+ 1. input_file- path for input file
+% % % # + 2. output_dir- path for output directory
+% % % # + 3. db_type- Database, either KEGG, GO, or GO_BP
 
 clear; close all; clc
-
 BasePath = getenv("BIOSUITE_HOME");
+
+%%%%% USER INPUT %%%%%
+%Input 1: Input file path
 input_file = "genescf_demo.txt";
+
+%Input 2: Output directory path 
 output_dir = BasePath + "/demo/output/genescf_output/";
+
+%Input 3: Database 
+db_type = "KEGG";
+%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%% DEMO %%%%%%%%
 mkdir(output_dir)
 str = "cp " + BasePath + "/demo/demo_data/" + input_file + " " + output_dir; system(str)
-db_type = "KEGG";
-
 genescf(input_file,output_dir,db_type)
+%%%%%%%%%%%%%%%%%%%%%%
