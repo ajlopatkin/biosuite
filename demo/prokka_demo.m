b06f22bf727cd1a82981d40cb581b647e2ad3957
csjohnson23 <75772305+csjohnson23@users.noreply.github.com>
2020-12-26 19:03:40 +0000

    Update prokka_demo.m

--- demo/prokka_demo.m
+++ demo/prokka_demo.m
@@ -1,15 +1,24 @@
 % % # INPUTS (IN ORDER): 
-% % #	+ 1. input_fasta - input file to annotate
-% % #	+ 2. output_dir - directory
-% % #	+ 3. prefix_name - file prefix
+% % #	+ 1. prefix_name - file prefix (must be same file name as in input_fasta)
+% % #	+ 2. input_fasta - Path for input file to be annotated
+% % #	+ 3. output_dir - Directory path for storing output files
 
 clear;close all;clc
-
-% run prokka
 BasePath = getenv("BIOSUITE_HOME");
-prefix_name = "demo_genome";
-input_fasta = BasePath + "/demo/demo_data/" + prefix_name + ".fasta";
-output_dir = BasePath + "/demo/output/prokka/";
-mkdir(output_dir)
-prokka(input_fasta,output_dir,prefix_name);
 
+%%%%% USER INPUT %%%%%
+%Input 1: Name of .fasta file (extracted from input file path)
+prefix_name = "demo_genome";
+
+%Input 2: Path of input .fasta file
+input_fasta = BasePath + "/demo/demo_data/" + prefix_name + ".fasta";
+
+%Input 3: Directory path for output 
+output_dir = BasePath + "/demo/output/prokka/";
+%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%% DEMO %%%%%%%%
+mkdir(output_dir)
+% run prokka
+prokka(input_fasta,output_dir,prefix_name);
+%%%%%%%%%%%%%%%%%%%%%%
