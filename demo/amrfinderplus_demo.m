1633f3d7ac7fd8305431cb0d5110202b0902f0d9
csjohnson23 <75772305+csjohnson23@users.noreply.github.com>
2020-12-26 16:29:25 +0000

    Update amrfinderplus_demo.m

--- demo/amrfinderplus_demo.m
+++ demo/amrfinderplus_demo.m
@@ -2,12 +2,19 @@
 % from nucleotide data
 % % # INPUTS (IN ORDER): 
 % % #	+ 1. fasta_file - path to input fasta file
-% % #	+ 2. db_flag - p or n
-% % #	+ 3. output_file - file for output data
+% % #	+ 2. db_flag - 'p' for protein, or 'n' for nucleotide
+% % #	+ 3. output_file - path for output data file (must be .txt)
     
 BasePath = getenv("BIOSUITE_HOME");
-fasta_file = BasePath + "/demo/demo_data/demo_genome.fasta";
-db_flag = 'n';
-output_file = BasePath + "/demo/output/AMRfinderplus_demo.txt";
 
+%Input 1: Path to input fasta file
+fasta_file = BasePath + "/demo/output/fasta_downloads/SAMD00056130.fna";
+
+%Input 2: Database flag
+db_flag = 'n';
+
+%Input 3: Path to output file
+output_file = BasePath + "/demo/output/AMRfinderplus_SAMD00056130.txt";
+
+%Run amrfinderplus
 amrfinderplus(fasta_file,db_flag,output_file)
