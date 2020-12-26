27caf64017b19c55e612d895775a136ad5901261
csjohnson23 <75772305+csjohnson23@users.noreply.github.com>
2020-12-26 20:43:42 +0000

    Update traits_table_demo.m

--- demo/traits_table_demo.m
+++ demo/traits_table_demo.m
@@ -6,25 +6,39 @@
 % first column is the plasmid accession number. Then, the categories are
 % ST, epi_type, and Mobility.
 
-clear, close all, clc
+% % # INPUTS (IN ORDER):
+% % #	+ 1. input_file - path to input metadata file
+% % #	+ 2. ColsToKeep - List of column names from metadata to keep in 
+% % #         traits table
+% % #	+ 3. output_file - path for output file to store traits table
 
-%% input
-% define path to metadata and read in the table
+clear, close all, clc
 BasePath = getenv("BIOSUITE_HOME");
+
+%%%%% USER INPUT %%%%%
+%Input 1: path to input metadata file
 input_data_file = BasePath + "/demo/demo_data/metadata.xlsx";
+
+%Input 2: Column names to keep
+ColsToKeep = ["plasmidID_reduced","ST","epi_type","transferrability"];
+
+%Input 3: Path for output file to store traits table
+output_file = BasePath+"/demo/output/traits.csv"
+%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%% DEMO %%%%%%%%
+% read in the table from metadata
 T = readtable(input_data_file);
 
 %% determine and store new table of traits to keep
-% define the column names to keep. These correspond exactly to the column
-% names of the columns in our metadata. Loop through our table to find the
+% define the column names to keep. Loop through table to find the
 % column indices, and create a subset.
-ColsToKeep = ["plasmidID_reduced","ST","epi_type","transferrability"];
 ind = [];
 for q = 1:length(ColsToKeep)
     ind(end+1) = find(strcmp(T.Properties.VariableNames,ColsToKeep(q)));
 end
 T_traits = T(:,ind);
 
-%% output
 % write new table as traits file
-writetable(T_traits,BasePath+"/demo/output/traits.csv")
+writetable(T_traits,output_file)
+%%%%%%%%%%%%%%%%%%%%%%
