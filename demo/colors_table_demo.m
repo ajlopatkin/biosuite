922f3fbcfdd080d08dba41ad4e5e86ec5851fbdd
csjohnson23 <75772305+csjohnson23@users.noreply.github.com>
2020-12-26 17:24:41 +0000

    Update colors_table_demo.m

--- demo/colors_table_demo.m
+++ demo/colors_table_demo.m
@@ -1,24 +1,43 @@
 % This demo converts an input traits table into a traits_color table, where
 % each unique category from every trait is converted into a color for
-% heatmpa plotting
+% heatmap plotting
+% % # INPUTS (IN ORDER): 
+% % #	+ 1. input_file - file path to input metadata table  
+% % #	+ 2. output_file - path for output .txt file
+% % # COLOR CUSTOMIZATION: 
+% % #	+ 1. colors1: define colors for category 1 (ex. Sequence Type)
+% % #	+ 2. colors2: define colors for category 2, if applicable
+% % #	+ 3. colors3: define colors for category 3, if applicable
 
-clear, close all, clc
+clear;close all;clc
 
-%% input
-% define path to metadata and read in the table
 BasePath = getenv("BIOSUITE_HOME");
-input_data_file = BasePath + "/demo/demo_data/traits_demo.csv";
-T_traits = readtable(input_data_file);
 
-%% define colors for each category
-% generate colors file from phenotype file
-% see link for color options: https://python-graph-gallery.com/100-calling-a-color-with-seaborn/
-% mash_cluster.py clustergram options: https://medium.com/@morganjonesartist/color-guide-to-seaborn-palettes-da849406d44f
+%%%%% USER INPUT %%%%%%
+%Input 1: define path to metadata and read in the table
+input_data_file = BasePath + "/demo/demo_data/traits_demo.csv";
+
+%Input 2: Path for output .txt file
+output_file = BasePath + "/demo/output/traits_color.csv";
+%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%% COLOR CUSTOMIZATION %%%
+% Color options: https://python-graph-gallery.com/100-calling-a-color-with-seaborn/
+% Clustergram options: https://medium.com/@morganjonesartist/color-guide-to-seaborn-palettes-da849406d44f
+
 colors1 = {'blue' 'crimson' 'gold' 'mediumseagreen' 'blueviolet' 'black'...
     'darkgrey' 'greenyellow' 'mediumspringgreen' 'gold'};
-colors2 = {'white' 'black'};
-colors3 = {'red' 'yellow' 'blue'};
 
+colors2 = {'white' 'black'};
+
+colors3 = {'red' 'yellow' 'blue'};
+%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%% DEMO %%%%%%%%%%
+%read in input table
+T_traits = readtable(input_data_file);
+
+% generate colors file from phenotype file
 ST = categorical(string(T_traits.ST),string(unique(T_traits.ST)),...
     colors1(1:length(unique(T_traits.ST))));
 Epi = categorical(string(T_traits.epi_type),string(unique(T_traits.epi_type)),...
@@ -31,5 +50,7 @@ T_traits.ST = ST;
 T_traits.epi_type = Epi;
 T_traits.transferrability = Mobility;
 
-%% store in new table
-writetable(T_traits,BasePath+"/demo/output/traits_color.csv")
+% store color output in new table
+writetable(T_traits,output_file)
+%%%%%%%%%%%%%%%%%%%%%%%%%%
+
