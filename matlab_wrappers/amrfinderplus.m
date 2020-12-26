8901a50d8deda094207ea9cfbc1a6509934ff4fe
csjohnson23 <75772305+csjohnson23@users.noreply.github.com>
2020-12-26 21:08:15 +0000

    added organism argument

--- matlab_wrappers/amrfinderplus.m
+++ matlab_wrappers/amrfinderplus.m
@@ -1,12 +1,13 @@
-function amrfinderplus(fasta_file,db_flag,output_file)
+function amrfinderplus(fasta_file,db_flag,output_file,organism)
 
 % % # INPUTS (IN ORDER): 
 % % #	+ 1. fasta_file - path to input fasta file
 % % #	+ 2. db_flag - p or n
 % % #	+ 3. output_file - file for output data
+% % #	+ 4. organism - Taxon for point mutation screening
     
 str = "bash $BIOSUITE_HOME/bash_source/amrfinderplus.sh " +...
-    fasta_file + " " + db_flag + " " + output_file;
+    fasta_file + " " + db_flag + " " + output_file + " " + organism;
 
 [status,cmdout] = system(str);
 
