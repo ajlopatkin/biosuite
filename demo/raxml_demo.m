7fd3bf4ee7e7021430ade34918b8524ba981f756
ajlopatkin <alopatki@barnard.edu>
2021-01-19 10:03:55 -0500

    updated raxml wrappers and source code

--- demo/raxml_demo.m
+++ demo/raxml_demo.m
@@ -1,21 +1,17 @@
 % This demo builds a phylogenetic tree from multisequence alignment
 
 % % % # INPUTS (IN ORDER): 
-% % % #	+ 1. input_aln- Path for aligned input file 
-% % % # + 2. output_dir- Path for directory to store output newick file
+% % % #	+ 1. input_aln- input aligned file
+% % % # + 2. output_dir- output newick tree
+% % % # + 3. bootstraps- number of bootstrap repetitions
+% % % # + 4. OPTIONAL: outgroup- accession ID of outgroup to root the tree
 
 clear; close all; clc
+
 BasePath = getenv("BIOSUITE_HOME");
-
-%%%%% USER INPUT %%%%%
-%Input 1: path to aligned input file
-input_aln = BasePath + "/demo/demo_data/sampleAA_sucA.phy";
-
-%Input 2: Directory path for output files
+file_name = "sampleAA_sucA";
+input_aln = BasePath + "/demo/demo_data/" + file_name + ".phy";
 output_dir = BasePath + "/demo/output/raxml/";
-%%%%%%%%%%%%%%%%%%%%%%
-
-%%%%%%%% DEMO %%%%%%%%
 mkdir(output_dir)
-raxml(input_aln,output_dir)
-%%%%%%%%%%%%%%%%%%%%%%
+bootstraps = 100; 
+raxml(input_aln,output_dir,bootstraps)
--- matlab_wrappers/raxml.m
+++ matlab_wrappers/raxml.m
@@ -1,11 +1,24 @@
-function raxml(input_file,output_dir)
+function raxml(input_file, output_dir, bootstraps, outgroup)
 
-str = "bash $BIOSUITE_HOME/bash_source/raxml.sh "...
-    + input_file + " " + output_dir;
 
-[status,cmdout] = system(str);
+if nargin == 3
+    
+    str = "bash $BIOSUITE_HOME/bash_source/raxml.sh "...
+        + input_file + " " + output_dir + " " + bootstraps + " " + outgroup;
+    [status,cmdout] = system(str);
+    if status ~= 0
+        disp("Error running RaxML! Output below...")
+        disp(cmdout)
+    end
+    
+else
 
-if status ~= 0
-    disp("Error running RaxML! Output below...")
-    disp(cmdout)
+    str = "bash $BIOSUITE_HOME/bash_source/raxml.sh "...
+        + input_file + " " + output_dir +  " " + bootstraps;
+    [status,cmdout] = system(str);
+    if status ~= 0
+        disp("Error running RaxML! Output below...")
+        disp(cmdout)
+    end
+    
 end
