df796a92cdd082361abf53e32211aabdbce4ac50
csjohnson23 <75772305+csjohnson23@users.noreply.github.com>
2021-05-09 19:11:18 -0400

    resistance pipeline bash source

---
+++ bash_source/resistance_pipeline.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# SCRIPT NAME: amrfinderplus.sh
+# DESCRIPTION: Finds gene 
+# 
+# INPUTS (IN ORDER): 
+#	+ 1. fasta_file - path to input fasta file
+#	+ 2. db_flag - p or n
+#	+ 3. output_file - file for output data
+# 
+# DATE UPDATED: 3/24/21
+# AUTHOR: Claire Johnson
+
+#########################
+#~- ENVIRONMENT SETUP -~#
+#########################
+
+# Get the Conda source path
+CONDA_BASE=$(conda info --base)
+
+# Activate the conda environment
+source $CONDA_BASE/etc/profile.d/conda.sh
+
+# Parse the inputs
+input_dir=$1
+output_dir=$2
+species=$3
+
+###################
+#~- MAIN SCRIPT -~#
+###################
+
+python "$BIOSUITE_HOME"/bash_source/resistance_pipeline.py -i "$input_dir" -o "$output_dir" -t "$species"
