ffef56e060cc50e3fcdb1c79ceef7cffb5536015
csjohnson23 <75772305+csjohnson23@users.noreply.github.com>
2021-05-09 19:25:26 -0400

    Update resistance_pipeline.m

--- demo/resistance_pipeline.m
+++ demo/resistance_pipeline.m
@@ -1,18 +1,29 @@
-%%Resistance Gene Consensus Search Pipeline
+%%Resistance Gene Search Pipeline
+% % % # INPUTS (IN ORDER): 
+% % % #	+ 1. id_list: List of biosample ids to run through the pipeline
+% % % #	+ 2. base_folder: Path to directory to read/save intermediate and final pipeline outputs
 
 clear;close all;clc
 BasePath = getenv("BIOSUITE_HOME");
+
+% % Input 1: List of biosample/accession IDs
 id_list =["Ex003", "Ax270"];
+
+% % Input 2: Directory to hold outputs and folders created at each pipeline step
 base_folder = BasePath + "/demo/ab2/";
-mkdir(base_folder)
-consensus_folder = base_folder + "consensi/";
+
+% % Input 3: Species flag. a: Acinetobacter, 
 species = "a";
+
+%%%%%%%%%%%% PIPELINE %%%%%%%%%%
+mkdir(base_folder)
+consensus_folder = base_folder + "consensus/";
 mkdir(consensus_folder)
 i = 1;
 while i <= length(id_list)
     id = id_list(i);
     disp(id)
-    %Make id Folder
+    %Make ID Folder
     id_folder = base_folder + id + "/";
     mkdir(base_folder + id);
     %Download
@@ -34,3 +45,5 @@ str = "bash $BIOSUITE_HOME/bash_source/resistance_pipeline.sh " + consensus_fold
      disp("Error running pipeline! Output below...")
      disp(cmdout)
  end
+ 
+%%%%%%%%%% END PIPELINE %%%%%%%%%%
