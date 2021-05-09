656ed500414d3656892d242a3917c0bfc567e343
csjohnson23 <75772305+csjohnson23@users.noreply.github.com>
2021-05-09 18:46:42 -0400

    phylo tree pipeline matlab wrappers

--- matlab_wrappers/snippy_multi.m
+++ matlab_wrappers/snippy_multi.m
@@ -1,46 +1,12 @@
-function snippy_multi(input_dir,ref_file,output_dir)
+function snippy_multi(input_dir_single, input_dir_paired,ref_file,output_dir)
+BasePath = getenv("BIOSUITE_HOME");
+config_out_path = output_dir + "snippy_input.tab";
 
-curr_dir = cd(output_dir);
-s = dir(input_dir);
-names = {s.name}';
-s(strcmp(names,".") | strcmp(names,"..") | contains(names,"DS")) = [];
-output_tab = output_dir + "list.tab";
+str = "bash " + BasePath + "/bash_source/snippy_config.sh " + input_dir_single + " " + input_dir_paired + " " + config_out_path + " " + ref_file + " " + output_dir;
 
-if isfile(output_tab)
-    delete(output_tab);
-end
-
-for k = 1:length(s)
-    if strcmp(s(k).name, ".") || strcmp(s(k).name, "..")
-        continue
-    end
-    
-    curr_file = s(k).folder + "/" + s(k).name;
-    
-    if strcmp(curr_file, ref_file)
-        continue
-    end
-    
-    curr_name = split(s(k).name,".");
-    curr_name = curr_name{1};
-    
-    fid = fopen(output_tab, "a+");
-    curr_line = curr_name + "	" + curr_file+"\n";
-    fprintf(fid, curr_line);
-    fclose(fid);
-end
-
-str = "bash $BIOSUITE_HOME/bash_source/snippy_multi.sh " +...
-    output_tab + " " + ref_file + " " + output_dir;
-
-[status,cmdout] = system(str)
-
+[status,cmdout] = system(str);
+disp(cmdout)
 if status ~= 0
     disp("Error running Snippy! Output below...")
     disp(cmdout)
 end
-
-cd(curr_dir);
-
-end
-    
